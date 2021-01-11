645238549e4207ed4a938394764b9f84629e05ac
cmak8888 <calvin.mak8888@gmail.com>
2021-01-10 18:39:49 -0700

    Forgot to add files

---
+++ src/test/resources/com/revature/services/TicketService.java
@@ -0,0 +1,17 @@
+package com.revature.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicketService {
+
+	@InjectMocks
+	TicketsDao tDao = new TicketsHandler();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
